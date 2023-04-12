b9aeb9287bd96c265615f724efb985c038bb1c4b
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-04-12 13:41:35 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose"
-import listEndpoints from "express-list-endpoints"
-import server from "./server.js"
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+import server from "./server.js";
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
 mongoose.connection.on("connected", () => {
-  console.log(`✅ Successfully connected to Mongo!`)
+  console.log(`✅ Successfully connected to Mongo!`);
   server.listen(port, () => {
-    console.table(listEndpoints(server))
-    console.log(`✅ Server is running on port ${port}`)
-  })
-})
+    console.table(listEndpoints(server));
+    console.log(`✅ Server is running on port ${port}`);
+  });
+});
